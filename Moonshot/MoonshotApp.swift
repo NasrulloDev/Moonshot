d9c809c5f5aa69237469cd1be64429ad823e66ef
unknown7761 <ismoilzanovnasrullo@gmail.com>
2024-02-05 00:57:28 +0500

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Насрулло Исмоилжонов on 05/02/24.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
